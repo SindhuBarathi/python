8eca4736f79950398b5442bea667eb018f5fc76f
Sindhuphython <sindhubarathiak@gmail.com>
2021-07-20 20:37:28 +0530

    list DS

--- list.py
+++ list.py
@@ -1,9 +1,2 @@
 names=["sindhu","jayasree","siveha","raksh",23,23,5,100]
-print(names)
-print(len(names)) # len is a function used to check the length of list 
-# now the next we hv to delete the element from list okk
-# there are 2,3 inbuild function for deletion 
-# del name[1] # this statement delete the element store at 1 index 
-del names[1]
-names.pop(1)
-print(names)
+print(names[2:4])
