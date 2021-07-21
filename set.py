3d29604dd4f06ed285bc757f8fe0bfbfa9ad279b
Sindhuphython <sindhubarathiak@gmail.com>
2021-07-21 14:30:53 +0530

    set

---
+++ set.py
@@ -0,0 +1,6 @@
+fruits={"orange","apple","manago"}
+juice=("orange")
+# print(fruits.intersection(juice))
+print(juice.issubset(fruits))
+print(juice.issuperset(fruits))
+print(fruits.difference(juice))
---
+++ student.py
@@ -0,0 +1,6 @@
+data=[{"name":"sindhu","rollnum":20,"adminum":5041,"college":"knsit"},{"name":"sin","rollnum":89,"adminum":5042,"college":"bmsit"}]
+# print(data)
+# print(len(data))
+# print(data[0].keys())
+# print(data[0].values())
+print(data[0]["names"])
