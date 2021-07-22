0eb0d22b535513fa846c1976f69761636e61bde8
Sindhuphython <sindhubarathiak@gmail.com>
2021-07-22 12:01:08 +0530

    addition fun

---
+++ function.py
@@ -0,0 +1,10 @@
+# function definition
+# def sayHello():
+#     return "hello"
+# # function calling
+# print(sayHello())
+def sayHello(name):
+    return "hello"+name
+getname=input("enter name:")
+print(sayHello(getname))
+
---
+++ some.py
@@ -0,0 +1,5 @@
+def addNumbers(a,b):
+    return a+b
+n1=int(input("enter a number:"))
+n2=int(input("enter a number:"))
+print(addNumbers(n1,n2))
